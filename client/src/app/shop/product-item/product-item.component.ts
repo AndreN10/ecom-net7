2263b9baa9efeaeb635eecd8ebac523b193726b6
Andre Nguyen <andguen26179@gmail.com>
2023-05-08 12:47:42 +0700

    End of section 14

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,11 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
 import { Product } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent {
- @Input() product?: Product
+  @Input() product?: Product;
+
+  constructor(private basketService: BasketService) {}
+
+  addItemToBasket() { 
+    // only execute the right hand side if we have the product
+    this.product && this.basketService.addItemToBasket(this.product) 
+  }
 }
